deff61736b1eed23468cfa88a9ce6d3aa38d37c3
naif <nayef7138@gmail.com>
2022-06-11 08:49:14 +0300

    add: javascript functionality

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './index.scss';
+
+// close all details element
+
+const detailsElements = document.querySelectorAll('details');
+
+detailsElements.forEach((el) => {
+  let isDetailsOpen = false;
+  el.addEventListener('click', (event) => {
+    // re close when click
+    if (isDetailsOpen) {
+      setTimeout(() => {
+        el.removeAttribute('open');
+      }, 10);
+
+      isDetailsOpen = false;
+      return;
+    }
+
+    // close all open details
+    document
+      .querySelectorAll('[open]')
+      .forEach((el) => el.removeAttribute('open'));
+
+    isDetailsOpen = true;
+  });
+});
