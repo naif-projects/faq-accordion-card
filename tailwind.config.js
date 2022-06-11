f903fcedb65126d79af10236f8d207f0c6b93628
naif <nayef7138@gmail.com>
2022-06-11 08:49:21 +0300

    update: tailwind config | add custom color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-light': 'color_here',
-        'primary-base': 'color_here',
-        'primary-dark': 'color_here',
+        'primary-light': 'hsl(240, 5%, 91%)',
+        'primary-base': 'hsl(240, 6%, 50%)',
+        'primary-dark': 'hsl(237, 12%, 33%)',
+        'primary-darker': 'hsl(238, 29%, 16%)',
 
-        'secondary-light': 'color_here',
-        'secondary-base': 'color_here',
-        'secondary-dark': 'color_here',
+        'secondary-base': 'hsl(14, 88%, 65%)',
+
+        'soft-violet': 'hsl(273, 75%, 66%)',
+        'soft-blue': 'hsl(240, 73%, 65%)',
       },
 
       fontFamily: {
-        primary: ["'custom_font_here'", 'sans-serif'],
+        primary: ["'Kumbh Sans'", 'sans-serif'],
       },
     },
   },
